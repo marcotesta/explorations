b4fd4e3b388b6f66832f49dbdbb656121051cc2f
marco <mrc.tst@gmail.com>
2016-05-01 19:25:01 +0200

    Rename class and remove dependency from input file

--- src/main/java/it/mondogrua/exploration/ReadNewlyAddedLinesToAFile.java
+++ src/main/java/it/mondogrua/exploration/ReadNewlyAddedLinesToAFile.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class ReadOnlyNewlyAddedLinesToAFile {
+public class ReadNewlyAddedLinesToAFile {
 
     private static final int DEFAULT_RETRY_DELAY = 1000;
     private static final int DEFAULT_BUFFER_SIZE = 4096;
@@ -22,24 +22,20 @@ public class ReadOnlyNewlyAddedLinesToAFile {
         void fileNotFound();
     }
 
-    private final String fileName;
+    private final RandomAccessFile file;
     private final TailListener listener;
-    private final ReaderOpenerListener openerListener;
+    private volatile boolean run = true;
 
-    public ReadOnlyNewlyAddedLinesToAFile(final String aFileName,
-            final TailListener aListener, ReaderOpenerListener openerListener) {
-        this.fileName = aFileName;
+    public ReadNewlyAddedLinesToAFile(RandomAccessFile file,
+            final TailListener aListener) {
         this.listener = aListener;
-        this.openerListener = openerListener;
+        this.file = file;
     }
 
     public void readNewLines() {
-        RandomAccessFile file = null;
         try {
-            file = openFile(fileName, openerListener);
-            seefEndOfFile(file);
 
-            while (true) {
+            while (run) {
                 if (moreToRead(file)) {
                     readLines(file);
                 }
@@ -58,28 +54,12 @@ public class ReadOnlyNewlyAddedLinesToAFile {
         }
     }
 
-    private void seefEndOfFile(RandomAccessFile file) throws IOException {
-        file.seek(file.length());
+    public void stopReading() {
+        run = false;
     }
 
     private boolean moreToRead(RandomAccessFile file) throws IOException {
         return file.length() > file.getFilePointer();
-    }
-
-    private RandomAccessFile openFile(String fileName,
-            ReaderOpenerListener aOpenerListener) throws InterruptedException,
-                    IOException {
-        while (true) {
-            try {
-                RandomAccessFile file = new RandomAccessFile(fileName, "r");
-                if (file != null) {
-                    return file;
-                }
-            } catch (final FileNotFoundException e) {
-                aOpenerListener.fileNotFound();
-                Thread.sleep(DEFAULT_RETRY_DELAY);
-            }
-        }
     }
 
     private void readLines(final RandomAccessFile file) throws IOException {
@@ -102,17 +82,43 @@ public class ReadOnlyNewlyAddedLinesToAFile {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         if (args.length != 1) {
             System.out.println(
                     "Usage: java it.mondogrua.explorations.ReadOnlyNewlyAddedLinesToAFile <filename>");
             return;
         }
-        ReadOnlyNewlyAddedLinesToAFile tail =
-                new ReadOnlyNewlyAddedLinesToAFile(args[0],
-                        createTailListener(), createReaderOpenerListener());
+        String fileName = args[0];
+
+        RandomAccessFile file = openFile(fileName,
+                createReaderOpenerListener());
+        seekEndOfFile(file);
+
+        ReadNewlyAddedLinesToAFile tail = new ReadNewlyAddedLinesToAFile(file,
+                createTailListener());
         tail.readNewLines();
+    }
+
+    static RandomAccessFile openFile(String fileName,
+            ReaderOpenerListener aOpenerListener) throws InterruptedException,
+                    IOException {
+        while (true) {
+            try {
+                RandomAccessFile file = new RandomAccessFile(fileName, "r");
+                if (file != null) {
+                    return file;
+                }
+            } catch (final FileNotFoundException e) {
+                aOpenerListener.fileNotFound();
+                Thread.sleep(DEFAULT_RETRY_DELAY);
+            }
+        }
+    }
+
+    private static void seekEndOfFile(RandomAccessFile file)
+            throws IOException {
+        file.seek(file.length());
     }
 
     private static TailListener createTailListener() {
@@ -130,7 +136,7 @@ public class ReadOnlyNewlyAddedLinesToAFile {
         };
     }
 
-    private static ReaderOpenerListener createReaderOpenerListener() {
+    static ReaderOpenerListener createReaderOpenerListener() {
         return new ReaderOpenerListener() {
 
             @Override
